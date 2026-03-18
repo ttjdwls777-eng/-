e9d7392564803c324405aa514ef105cc84a0924a
ttjdwls777-eng <ttjdwls777@gmail.com>
2026-03-19 01:51:12 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// XGP ONLINE CONFIG
+
+window.XGP_ONLINE_CONFIG = {
+  enabled: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBDJxn1OEyQtGhJDemFA7pF-5QA-GGLW7Y",
+    authDomain: "xgp-minigame.firebaseapp.com",
+    projectId: "xgp-minigame",
+    storageBucket: "xgp-minigame.firebasestorage.app",
+    messagingSenderId: "712312742763",
+    appId: "1:712312742763:web:eef8675828aefe8c71222e"
+  }
+};
